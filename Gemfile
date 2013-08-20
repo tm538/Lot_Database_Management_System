29c8e53fca0b9705540934c154a5e1f4c2019a65
Tim J. Munn <tm538@york.ac.uk>
2013-08-20 15:33:39 +0100

    setup

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 #ruby-gemset=railstutorial_rails_4_0
 
-gem 'rails', '4.0.0rc'
+gem 'rails', '4.0.0.rc2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
@@ -14,7 +14,7 @@ group :test do
   gem 'capybara', '2.1.0'
 end
 
-gem 'sass-rails', '4.0.0rc2'
+gem 'sass-rails', '4.0.0.rc2'
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.0'
 gem 'jquery-rails', '2.2.1'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '1262d0c2cddff515ac867da0e5b44b757d53e119a74846687299c99c5b87545a7db81b248b1c1220c36cd835b6395acca661556165529922530e23dcfdec9199'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
