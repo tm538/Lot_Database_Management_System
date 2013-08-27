7b0f376c9d51bf1798c647d794d86b30b8110241
Tim J. Munn <tm538@york.ac.uk>
2013-08-27 10:12:44 +0100

    finished sing in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   #convert email to lowercase before saving to prevent duplication
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
   
   #Validate fields
   validates :name, presence: true, 
@@ -15,4 +16,19 @@ class User < ActiveRecord::Base
                     
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+    
 end
