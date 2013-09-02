86a85c33257440634dd23b5cba98108ebdd0a810
Tim J. Munn <tm538@york.ac.uk>
2013-09-02 17:30:45 +0100

    new lot prettyfield

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -2,7 +2,49 @@ class LotsController < ApplicationController
   
   def new
     @lot = Lot.new(:commercial => false, :client_id => 1, :created_at => Time.now)
-    @lot.data_entered_by = current_user.id
   end
+  
+    def show
+      @lot = Lot.find(params[:id])
+    end
+  
+  def create
+    @lot = Lot.new(lot_params)
+    @lot.data_entered_by = current_user.id
+    if @lot.save
+      flash[:success] = "lot " + " sucessfully created"
+      redirect_to 'new'
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+      @lot = Lot.find(params[:id])
+  end
+  
+  private
+
+    def lot_params
+      params.require(:lot).permit(:client_id, 
+                                  :commercial, 
+                                  :commonname_id, 
+                                  :analysis_by, 
+                                  :samp_id,
+                                  :sampletype_id,
+                                  :commonname_id,
+                                  :phylum,
+                                  :l_class,
+                                  :genus,
+                                  :species,
+                                  :sample_form,
+                                  :to_return,
+                                  :region,
+                                  :site,
+                                  :analysis_by,
+                                  :extra_info,
+                                  :created_at
+                                  )
+    end
   
 end
--- app/models/lot.rb
+++ app/models/lot.rb
@@ -2,5 +2,7 @@ class Lot < ActiveRecord::Base
 
   default_scope -> { order('id DESC') }
   validates :client_id, presence: true 
-  validates :commercial, presence: true
+  validates :commercial, :inclusion => {:in => [true, false]}
+  validates :commonname_id, presence: true
+  validates :analysis_by, presence: true
 end
