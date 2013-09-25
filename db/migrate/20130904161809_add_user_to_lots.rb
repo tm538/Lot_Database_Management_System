95477e7b63a4b8f5be3f9ef5e95642a349cd1200
Tim J. Munn <tm538@york.ac.uk>
2013-09-25 12:47:20 +0100

    removed redundant migrations

--- db/migrate/20130904161809_add_user_to_lots.rb
+++ db/migrate/20130904161809_add_user_to_lots.rb
@@ -1,7 +1,5 @@
 class AddUserToLots < ActiveRecord::Migration
   def change
-    change_column :searches, :user_id, :integer
     add_column :lots, :user_id, :integer
-    remove_column :lots, :lot_id
   end
 end
