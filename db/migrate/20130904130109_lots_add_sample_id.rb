30b5b284c2931b789fc69e5649ec9d146c487dcf
Tim J. Munn <tm538@york.ac.uk>
2013-09-25 12:18:37 +0100

    removed uneeded coloumn

--- db/migrate/20130904130109_lots_add_sample_id.rb
+++ db/migrate/20130904130109_lots_add_sample_id.rb
@@ -1,7 +1,5 @@
 class LotsAddSampleId < ActiveRecord::Migration
   def change
-    add_column :lots, :lot_id, :integers
-    add_index :searches, [:id]
-    
+    add_index :searches, [:id]    
   end
 end
